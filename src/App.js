2f2d005e4b0e791d8be95900e89e511f1ac60edc
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-02-22 23:29:59 +0100

    Completed hw 72

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderNavigation from "./components/HeaderNavigation";
+import LeftNav from "./components/LeftNav";
+import Content from "./components/Content";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <HeaderNavigation/>
+            <Container>
+                <Row className='mt-3'>
+                    <Col xs lg="2" className='nav-background'><LeftNav/></Col>
+                    <Col><Content/></Col>
+                </Row>
+            </Container>
     </div>
   );
 }
